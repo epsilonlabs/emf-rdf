1e33be0b33db571d2fa0d61ed7992357fb23ec84
Owen Reynolds <owen.reynolds@york.ac.uk>
2025-01-22 16:44:05 +0000

    First draft of the Platform:/ to File:/ conversion process

--- bundles/org.eclipse.epsilon.emc.rdf.dt/src/org/eclipse/epsilon/emc/rdf/dt/EclipseRDFModel.java
+++ bundles/org.eclipse.epsilon.emc.rdf.dt/src/org/eclipse/epsilon/emc/rdf/dt/EclipseRDFModel.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
@@ -15,16 +17,68 @@ public class EclipseRDFModel extends RDFModel {
 	@Override
 	protected void loadModel() throws EolModelLoadingException { 
 		// Change any platform:/ URLs to file:/ URLs in these lists...
-		System.err.println("EclipseRDFModel");
-		schemaURIs.forEach(e -> System.out.println(e));
-		dataURIs.forEach(e -> System.out.println(e));
+
+		processEclipsePlatformUrlsInList(schemaURIs);
+		processEclipsePlatformUrlsInList(dataURIs);
 		
-		examplePlatformToFile();
-		
-		// Call the RDFModel load as normal with File URLs
+		// Call the RDFModel load as normal with only File:/ URLs in the lists
 		super.loadModel();
-		
 	}
+	
+	// Pushes a list of URLs through a process to turn any Platform:/ into File:/ which Jena can use
+	private void processEclipsePlatformUrlsInList(List<String> urlList) {
+		List<String> goodUrls = new ArrayList<String>();
+		
+		urlList.forEach(u -> {
+			String newUrlString = processPlatformURLtoFileUrl(u);
+			if (newUrlString != null) {
+				goodUrls.add(newUrlString);
+			}
+		});
+		urlList.clear();
+		urlList.addAll(goodUrls);
+	}
+	
+
+	// Will attempt to resolve a String to a URI and then URL, gets the file system path and returns it
+	// A File:/ URL is unchanged by this process, Platform:/ URLs become File:/ URLs
+	private String processPlatformURLtoFileUrl(String urlString) {
+		
+		URI fileUri = URI.create(urlString);
+		
+		URL fileUrl = null;
+		try {
+			fileUrl = fileUri.toURL();		
+			// System.err.println("[OK] fileUrl: " + fileUrl);
+		} catch (MalformedURLException e) {
+			//e.printStackTrace();
+			System.err.println("fileUri.toURL() - " + urlString);
+		}
+		
+		URL fileSystemPathUrl = null;		
+		try {
+			fileSystemPathUrl = FileLocator.toFileURL(fileUrl);
+			// System.err.println("[OK] fileSystemPathUrl: " + fileSystemPathUrl);
+		} catch (IOException e) {
+			//e.printStackTrace();
+			System.err.println("FileLocator.toFileURL(fileUrl); - " + fileUrl);
+		}
+		
+		if (null != fileSystemPathUrl) {
+			return fileSystemPathUrl.toString();
+		} else {
+			return null;
+		}
+	}
+
+	// Handy way to show the lists on the console
+	private void showList (String listName, List<String> list) {
+		System.out.println(listName + ": ");
+		list.forEach(e -> System.out.println(e));
+	}
+	
+	
+	// Stuff to delete later
 	
 	private static final String PLATFORM_URL_OWLDATA = "platform:/resource/org.eclipse.epsilon.examples.emc.rdf.OWLdata/owlDemoData.ttl";
 	private static final String PLATFORM_URL_OWLSCHEMA = "platform:/resource/org.eclipse.epsilon.examples.emc.rdf.OWLdata/owlDemoData.ttl";
@@ -41,6 +95,7 @@ public class EclipseRDFModel extends RDFModel {
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.err.println(readOnLoad);
 		}
 		
 		URL fileSystemPathUrl = null;		
