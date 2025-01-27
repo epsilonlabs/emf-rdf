a544a0220487bb82764d106961d4bd30661107e4
Owen Reynolds <owen.reynolds@york.ac.uk>
2025-01-27 15:44:35 +0000

    Tests and Project test harness

---
+++ tests/org.eclipse.epsilon.emc.rdf.tests/src/org/eclipse/epsilon/emc/rdf/EclipseProjectEnvTest.java
@@ -0,0 +1,124 @@
+package org.eclipse.epsilon.emc.rdf;
+
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.Path;
+import org.junit.After;
+import org.junit.Before;
+public class EclipseProjectEnvTest {
+
+/**
+ * <p>
+ * Base class for tests requiring an Eclipse IDE project and workbench environment.
+ * </p>
+ * 
+ * <p>
+ * Note: all tests based on this class must run as JUnit Plug-In tests, not as
+ * regular tests, and the ui.workbench product or Headless needs to be run. We need a
+ * working, open workbench for these tests.
+ * </p>
+ */
+	
+	private final String projectUrl;
+	
+	private IProject testProject;
+
+	/**
+	 * Creates a new project with this URL
+	 * 
+	 * @param projectUrl
+	 *            Project URL to use for a project resource in Eclipse IDE Workbench used for the test
+	 */
+	public EclipseProjectEnvTest(String projectUrl) {
+		this.projectUrl = projectUrl;
+	}
+
+	@Before
+	public void createTestProject() throws Exception {
+		final IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		final IWorkspaceRoot root = workspace.getRoot();
+		
+		testProject = root.getProject(projectUrl);
+		if (testProject.exists()) {
+			deleteTestProject();
+		}
+		testProject.create(null);
+		testProject.open(null);		
+		
+		//System.out.println("Test location URI: "+testProject.getLocationURI());		
+	}
+
+	@After
+	public void deleteTestProject() throws Exception {
+		testProject.delete(true, true, null);
+		
+		int count = 0;
+		while (!testProject.isSynchronized(1)) {
+			count = checkTimeOut(count, 10,"Waiting for delete sync... ");			
+		}
+	}
+	
+	public void copyIntoProject(String path) throws Exception {
+		//System.out.println("copyIntoProject() : "+path);
+		IFile destFile=null; 
+		try (InputStream source = getClass().getResourceAsStream(path)) {
+			destFile = testProject.getFile(new Path(path));
+			createParentFolders(destFile);
+			destFile.create(source, false, null);
+			//System.out.println("source: "+ path);
+			//System.out.println("destFile: "+ destFile.getFullPath());			
+		} catch (Exception e) {
+			System.out.println("ERROR: copyIntoProject() " +e);
+		}
+		if (null != destFile) {			
+			int count = 0;
+			while (!destFile.isSynchronized(1)) {	
+				count = checkTimeOut(count, 10,"Waiting for file sync");			
+			}
+		}
+	}
+	
+	private static void createParentFolders(IResource res) throws Exception {
+		final IContainer parent = res.getParent();
+		if (parent instanceof IFolder) {
+			createParentFolders(parent);
+		}
+		if (res instanceof IFolder && !res.exists()) {
+			((IFolder) res).create(false, true, null);
+		}
+	}
+		
+	// Delays 1 second, set limit to X seconds you want to wait
+	private int checkTimeOut(int current, int limit, String errorLabel) throws Exception {
+		System.out.println(" - " + errorLabel + " Time out: " + current + "/" + limit );
+		if (current >= limit)
+		{
+			//System.err.println("Check time out error: " + errorLabel);	
+			throw new Exception("Check time out error: " + errorLabel);
+		}
+		delaySeconds(1);
+		return ++current;		
+	}
+	
+	private void delaySeconds(int seconds) {
+		try {
+			TimeUnit.SECONDS.sleep(seconds);
+		} catch (InterruptedException ie) {
+			Thread.currentThread().interrupt();
+		}
+	}
+	
+	public String getProjectUrl() {
+		return projectUrl;
+	}
+	
+	public String getTestProjectURIString() {
+		return testProject.getLocationURI().toString();
+	}
+
+	public IProject getTestProject() {
+		return testProject;
+	}
+}
---
+++ tests/org.eclipse.epsilon.emc.rdf.tests/src/org/eclipse/epsilon/emc/rdf/EclipseRDFModelUrlTest.java
@@ -0,0 +1,153 @@
+/********************************************************************************
+ * Copyright (c) 2024 University of York
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Antonio Garcia-Dominguez - initial API and implementation
+ ********************************************************************************/
+
+package org.eclipse.epsilon.emc.rdf;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.eclipse.epsilon.common.util.StringProperties;
+import org.eclipse.epsilon.emc.rdf.dt.EclipseRDFModel;
+import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
+import org.eclipse.epsilon.eol.execute.context.EolContext;
+import org.junit.After;
+import org.junit.Test;
+
+public class EclipseRDFModelUrlTest extends EclipseProjectEnvTest {
+
+	public EclipseRDFModelUrlTest() {
+		super(PROJECT_URL);
+	}
+
+	private static final String PROJECT_URL = "myProject";
+
+	private static final String OWL_DEMO_DATAMODEL = "/resources/OWL/owlDemoData.ttl";
+	private static final String OWL_DEMO_SCHEMAMODEL = "/resources/OWL/owlDemoSchema.ttl";
+	private static final String LANGUAGE_PREFERENCE_EN_STRING = "en";
+	
+	private static final String URI_BIGNAME42 = "urn:x-hp:eg/bigName42";
+	private static final String URI_ALIENBOX51 = "urn:x-hp:eg/alienBox51";
+	private static final String URI_WHITEBOX = "urn:x-hp:eg/whiteBoxZX";
+
+	private EclipseRDFModel model;
+	private EolContext context;
+
+	@Test
+	public void relativePathModelLoad() throws EolModelLoadingException {
+		String dataUrl = new String("." + OWL_DEMO_DATAMODEL);
+		String schemaUrl = new String("." + OWL_DEMO_SCHEMAMODEL);
+		StringProperties props = createPropertyString(dataUrl, schemaUrl, LANGUAGE_PREFERENCE_EN_STRING);
+
+		this.context = new EolContext();
+		copyModelFiles();
+		model.load(props);
+	}
+
+	@Test
+	public void relativeFileUrlModelLoad() throws EolModelLoadingException {
+		String dataUrl = new String("file:." + OWL_DEMO_DATAMODEL);
+		String schemaUrl = new String("file:." + OWL_DEMO_SCHEMAMODEL);
+		StringProperties props = createPropertyString(dataUrl, schemaUrl, LANGUAGE_PREFERENCE_EN_STRING);
+
+		this.context = new EolContext();
+		copyModelFiles();
+		model.load(props);
+		loadedModelTest();
+	}
+
+	@Test
+	public void longFileUrlModelLoad() throws EolModelLoadingException {
+		String dataUrl = new String(getTestProjectURIString() + OWL_DEMO_DATAMODEL);
+		String schemaUrl = new String(getTestProjectURIString() + OWL_DEMO_SCHEMAMODEL);
+		StringProperties props = createPropertyString(dataUrl, schemaUrl, LANGUAGE_PREFERENCE_EN_STRING);
+
+		this.context = new EolContext();
+		copyModelFiles();
+		model.load(props);
+		loadedModelTest();
+
+	}
+
+	@Test
+	public void platformUrlModelLoad() throws EolModelLoadingException {
+		String dataUrl = new String("platform:/resource/" + PROJECT_URL + "/" + OWL_DEMO_DATAMODEL);
+		String schemaUrl = new String("platform:/resource/" + PROJECT_URL + "/" + OWL_DEMO_SCHEMAMODEL);
+		StringProperties props = createPropertyString(dataUrl, schemaUrl, LANGUAGE_PREFERENCE_EN_STRING);
+
+		this.context = new EolContext();
+		copyModelFiles();
+
+		model.load(props);
+		loadedModelTest();
+	}
+
+	@Test(expected = EolModelLoadingException.class)
+	public void missingResourceInPlatformUrlModelLoad() throws EolModelLoadingException {
+		String dataUrl = new String("platform:/-/" + PROJECT_URL + "/" + OWL_DEMO_DATAMODEL);
+		String schemaUrl = new String("platform:/-/" + PROJECT_URL + "/" + OWL_DEMO_SCHEMAMODEL);
+		StringProperties props = createPropertyString(dataUrl, schemaUrl, LANGUAGE_PREFERENCE_EN_STRING);
+
+		this.context = new EolContext();
+		copyModelFiles();
+		model.load(props);
+	}
+
+	@Test(expected = EolModelLoadingException.class)
+	public void missingProjectUrlInPlatformUrlModelLoad() throws EolModelLoadingException {
+		String dataUrl = new String("platform:/resource" + OWL_DEMO_DATAMODEL);
+		String schemaUrl = new String("platform:/resource" + OWL_DEMO_SCHEMAMODEL);
+		StringProperties props = createPropertyString(dataUrl, schemaUrl, LANGUAGE_PREFERENCE_EN_STRING);
+
+		this.context = new EolContext();
+		copyModelFiles();
+		model.load(props);
+	}
+
+	@After
+	public void teardown() {
+		if (model != null) {
+			model.dispose();
+		}
+	}
+	
+	private void loadedModelTest() {		
+		assertTrue(model != null);
+		RDFResource element = model.getElementById(URI_WHITEBOX);
+		Object motherBoard = element.getProperty("eg:motherBoard", context);
+		assertTrue("motherBoard has max cardinality of 1 should only have that value returned ",
+			motherBoard instanceof RDFResource);
+	}
+
+	private void copyModelFiles() {
+		try {
+			super.copyIntoProject(OWL_DEMO_DATAMODEL);
+			super.copyIntoProject(OWL_DEMO_SCHEMAMODEL);
+		} catch (Exception e) {
+			// e.printStackTrace();
+		}
+	}
+
+	protected StringProperties createPropertyString(String dataModelUri, String schemaModelUri,
+			String languagePreference) {
+		this.model = new EclipseRDFModel();
+		StringProperties props = new StringProperties();
+		props.put(RDFModel.PROPERTY_DATA_URIS, dataModelUri);
+		props.put(RDFModel.PROPERTY_SCHEMA_URIS, schemaModelUri);
+		props.put(RDFModel.PROPERTY_LANGUAGE_PREFERENCE, languagePreference);
+		System.out.println("props: " + props.toString());
+		return props;
+	}
+}
