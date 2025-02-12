d3e21bf8f29bf2534fa56acae4174bd02e32cfa1
Owen Reynolds <owen.reynolds@york.ac.uk>
2025-02-12 14:46:46 +0000

    Reverted to original structure, odd test result for 1 type of test

--- bundles/org.eclipse.epsilon.emc.rdf/src/org/eclipse/epsilon/emc/rdf/RDFModel.java
+++ bundles/org.eclipse.epsilon.emc.rdf/src/org/eclipse/epsilon/emc/rdf/RDFModel.java
@@ -22,7 +22,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.jena.ontology.OntModel;
-import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -33,6 +32,7 @@ import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.reasoner.ValidityReport;
+import org.apache.jena.reasoner.ValidityReport.Report;
 import org.apache.jena.vocabulary.RDF;
 import org.eclipse.epsilon.common.util.StringProperties;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
@@ -45,18 +45,47 @@ import org.eclipse.epsilon.eol.models.IRelativePathResolver;
 
 public class RDFModel extends CachedModel<RDFModelElement> {
 
-	public static final String PROPERTY_DATA_URIS = "uris";
-	public static final String PROPERTY_SCHEMA_URIS = "schemaUris";
 	public static final String PROPERTY_LANGUAGE_PREFERENCE = "languagePreference";
+
+	public static final String PROPERTY_SCHEMA_URIS = "schemaUris";
+	public static final String PROPERTY_DATA_URIS = "uris";
+
+	/**
+	 * One of the keys used to construct the first argument to
+	 * {@link #load(StringProperties, String)}.
+	 *
+	 * This key should be set to a comma-separated list of prefix=uri pairs that
+	 * should be added to the prefix->URI map of the loaded RDF resource. These
+	 * pairs will take precedence over existing pairs in the resource.
+	 */
 	public static final String PROPERTY_PREFIXES = "prefixes";
 	public static final String PROPERTY_VALIDATE_MODEL = "enableModelValidation";
 
 	public static final String VALIDATION_SELECTION_JENA = "jena";
 	public static final String VALIDATION_SELECTION_NONE = "none";
 	public static final String VALIDATION_SELECTION_DEFAULT = VALIDATION_SELECTION_JENA;
-	
-	// Model loading and properties are located at the bottom of this Class
-	
+
+	protected final List<String> languagePreference = new ArrayList<>();
+	protected final Map<String, String> customPrefixesMap = new HashMap<>();
+
+	// TODO add to this list to cover reasoner types in the ReasonerRegistry Class
+	public enum ReasonerType {
+		NONE,
+		OWL_FULL;
+	}
+
+	protected ReasonerType reasonerType = ReasonerType.NONE;
+
+	public ReasonerType getReasonerType() {
+		return reasonerType;
+	}
+
+	public void setReasonerType(ReasonerType rdfsReasonerType) {
+		this.reasonerType = rdfsReasonerType;
+	}
+
+	protected final List<String> schemaURIs = new ArrayList<>();
+	protected final List<String> dataURIs = new ArrayList<>();
 	protected OntModel model;
 
 	public RDFModel() {
@@ -65,6 +94,11 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 	
 	protected RDFResource createResource(Resource aResource) {
 		return new RDFResource(aResource, this);
+	}
+
+	@Override
+	public Object getEnumerationValue(String enumeration, String label) throws EolEnumerationValueNotFoundException {
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -80,21 +114,6 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 
 		// TODO Generic names in case there isn't an RDF.type relationship?
 		return null;
-	}
-	
-	@Override
-	public boolean hasType(String type) {
-		try {
-			getTypeResourceByName(type);
-			return true;
-		} catch (EolModelElementTypeNotFoundException e) {
-			return false;
-		}
-	}
-
-	@Override
-	protected Object getCacheKeyForType(String type) throws EolModelElementTypeNotFoundException {
-		return getTypeResourceByName(type);
 	}
 
 	private String getQualifiedTypeName(RDFResource typeResource) {
@@ -121,6 +140,11 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 	}
 
 	@Override
+	public void setElementId(Object instance, String newId) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
 	public boolean owns(Object instance) {
 		return instance instanceof RDFModelElement && ((RDFModelElement) instance).getModel() == this;
 	}
@@ -129,7 +153,121 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 	public boolean isInstantiable(String type) {
 		return false;
 	}
-	
+
+	@Override
+	public boolean hasType(String type) {
+		try {
+			getTypeResourceByName(type);
+			return true;
+		} catch (EolModelElementTypeNotFoundException e) {
+			return false;
+		}
+	}
+
+	@Override
+	public void load(StringProperties properties, IRelativePathResolver resolver) throws EolModelLoadingException {
+		super.load(properties, resolver);
+
+		/*
+		 * This method assumes that the StringProperties replaces all previous
+		 * configuration of this RDFModel. This is the same as in other popular
+		 * EMC drivers (e.g. the EmfModel class).
+		 */
+		loadCommaSeparatedProperty(properties, PROPERTY_DATA_URIS, this.dataURIs);
+		loadCommaSeparatedProperty(properties, PROPERTY_SCHEMA_URIS, this.schemaURIs);
+		loadPropertyValidateModel(properties);
+
+		this.customPrefixesMap.clear();
+		String sPrefixes = properties.getProperty(PROPERTY_PREFIXES, "").strip();
+		if (!sPrefixes.isEmpty()) {
+			for (String sItem : sPrefixes.split(",")) {
+				int idxEquals = sItem.indexOf('=');
+				if (idxEquals <= 0 || idxEquals == sItem.length() - 1) {
+					throw new IllegalArgumentException(String.format("Entry '%s' does not follow the prefix=uri format", sItem));
+				}
+
+				String sPrefix = sItem.substring(0, idxEquals);
+				String sURI = sItem.substring(idxEquals + 1);
+				customPrefixesMap.put(sPrefix, sURI);
+			}
+		}
+
+		this.languagePreference.clear();
+		String sLanguagePreference = properties.getProperty(PROPERTY_LANGUAGE_PREFERENCE, "").strip();
+		if (!sLanguagePreference.isEmpty()) {
+			for (String tag : sLanguagePreference.split(",")) {
+				tag = tag.strip();
+				if (isValidLanguageTag(tag)) {
+					this.languagePreference.add(tag);
+				} else {
+					throw new EolModelLoadingException(
+						new IllegalArgumentException(
+							String.format("'%s' is not a valid BCP 47 tag", tag)
+						), this);
+				}
+			}
+		}
+
+		load();
+		
+		// After Loading all scheme, data models and inferring the full model, validate
+		if (validationProperty.contains(VALIDATION_SELECTION_JENA)) {
+			try {
+				validateModel();
+			} catch (Exception e) {
+				throw new EolModelLoadingException(e, this);
+			}
+		}
+	}
+
+	protected void loadCommaSeparatedProperty(StringProperties properties, String propertyName, List<String> targetList) {
+		targetList.clear();
+		{
+			String sUris = properties.getProperty(propertyName, "").strip();
+			if (!sUris.isEmpty()) {
+				for (String uri : sUris.split(",")) {
+					targetList.add(uri.strip());
+				}
+			}
+		}
+	}
+
+	@Override
+	public boolean store(String location) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean store() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	protected Collection<RDFModelElement> allContentsFromModel() {
+		final List<RDFModelElement> elems = new ArrayList<>();
+
+		for (ResIterator it = model.listSubjects(); it.hasNext(); ) {
+			Resource stmt = it.next();
+			elems.add(createResource(stmt));
+		}
+		
+		return elems;
+	}
+
+	@Override
+	protected Collection<RDFModelElement> getAllOfTypeFromModel(String type)
+			throws EolModelElementTypeNotFoundException {
+		Resource typeR = getTypeResourceByName(type);
+
+		ResIterator itInstances = model.listResourcesWithProperty(RDF.type, typeR);
+		List<RDFModelElement> instances = new ArrayList<>();
+		while (itInstances.hasNext()) {
+			instances.add(createResource(itInstances.next()));
+		}
+
+		return instances;
+	}
+
 	protected Resource getTypeResourceByName(String type) throws EolModelElementTypeNotFoundException {
 		RDFQualifiedName qName = RDFQualifiedName.from(type, this::getNamespaceURI);
 
@@ -159,49 +297,6 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 
 		throw new EolModelElementTypeNotFoundException(this.getName(), type);
 	}
-	
-	//
-	// GET ALL ___ FROM MODEL
-	
-	@Override
-	public Collection<String> getAllTypeNamesOf(Object instance) {
-		List<String> types = new ArrayList<>();
-
-		if (instance instanceof RDFResource) {
-			RDFResource res = (RDFResource) instance;
-			for (RDFResource t : res.getTypes()) {
-				types.add(getQualifiedTypeName(t));
-			}
-		}
-
-		return types;
-	}
-	
-	@Override
-	protected Collection<RDFModelElement> allContentsFromModel() {
-		final List<RDFModelElement> elems = new ArrayList<>();
-
-		for (ResIterator it = model.listSubjects(); it.hasNext(); ) {
-			Resource stmt = it.next();
-			elems.add(createResource(stmt));
-		}
-		
-		return elems;
-	}
-
-	@Override
-	protected Collection<RDFModelElement> getAllOfTypeFromModel(String type)
-			throws EolModelElementTypeNotFoundException {
-		Resource typeR = getTypeResourceByName(type);
-
-		ResIterator itInstances = model.listResourcesWithProperty(RDF.type, typeR);
-		List<RDFModelElement> instances = new ArrayList<>();
-		while (itInstances.hasNext()) {
-			instances.add(createResource(itInstances.next()));
-		}
-
-		return instances;
-	}
 
 	@Override
 	protected Collection<RDFModelElement> getAllOfKindFromModel(String kind)
@@ -211,48 +306,11 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 		return getAllOfTypeFromModel(kind);
 	}
 
-	//
-	// MODEL LIFE CYCLE (LOAD/DISPOSE/STORE)
-	
 	@Override
-	public void load(StringProperties properties, IRelativePathResolver resolver) throws EolModelLoadingException {
-		super.load(properties, resolver);
-
-		/*
-		 * This method assumes that the StringProperties replaces all previous
-		 * configuration of this RDFModel. This is the same as in other popular
-		 * EMC drivers (e.g. the EmfModel class).
-		 */
-		loadProperty(properties, PROPERTY_DATA_URIS, this.dataURIs);
-		loadProperty(properties, PROPERTY_SCHEMA_URIS, this.schemaURIs);
-		loadPropertyPrefixes(properties);
-		loadPropertyLanguagePreference(properties);
-		loadPropertyValidateModel(properties);
-
-		load();
-		
-		// After Loading all scheme, data models and inferring the full model, validate
-		if (validationProperty.contains(VALIDATION_SELECTION_JENA)) {
-			try {
-				validateModel();
-			} catch (Exception e) {
-				throw new EolModelLoadingException(e, this);
-			}
-		}
+	protected RDFResource createInstanceInModel(String type) throws EolModelElementTypeNotFoundException, EolNotInstantiableModelElementTypeException {
+		throw new UnsupportedOperationException();
 	}
 
-	private void loadProperty(StringProperties properties, String propertyName, List<String> targetList) {
-		targetList.clear();
-		{
-			String sUris = properties.getProperty(propertyName, "").strip();
-			if (!sUris.isEmpty()) {
-				for (String uri : sUris.split(",")) {
-					targetList.add(uri.strip());
-				}
-			}
-		}
-	}
-	
 	@Override
 	protected void loadModel() throws EolModelLoadingException {
 		if (!this.readOnLoad) {
@@ -281,7 +339,7 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 			}
 
 			//Create an OntModel to handle the data model being loaded or inferred from data and schema
-			this.model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM_RULE_INF);
+			this.model = ModelFactory.createOntologyModel();
 			
 			if (reasonerType == ReasonerType.NONE) {
 				// Only the OntModel bits are added to the dataModel being loaded.
@@ -301,9 +359,6 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 			throw new EolModelLoadingException(ex, this);
 		}
 	}
-	
-	//
-	// VALIDATE MODEL
 
 	private void validateModel() throws Exception {
 		/*
@@ -317,7 +372,7 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 			// Throw error with a string containing the validity report 
 			String reportString = "The loaded model is not valid or not clean\n";
 			int i = 1;
-			for (Iterator o = modelValidityReport.getReports(); o.hasNext();) {
+			for (Iterator<Report> o = modelValidityReport.getReports(); o.hasNext();) {
 				ValidityReport.Report report = (ValidityReport.Report) o.next();
 				reportString = reportString.concat(" " + i + ", " + report.toString());
 				i++;
@@ -326,45 +381,45 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 		}
 	}
 		
+	protected String validationProperty = VALIDATION_SELECTION_DEFAULT;
+	
+	private void loadPropertyValidateModel(StringProperties properties) {
+		validationProperty = properties.getProperty(RDFModel.PROPERTY_VALIDATE_MODEL, VALIDATION_SELECTION_JENA);
+	}
+	
+	public String getValidationProperty () {
+		return validationProperty;
+	}
+		
 	@Override
 	protected void disposeModel() {
 		model = null;
 	}
-	
-	// Store support coming soon...
+
 	@Override
-	public boolean store(String location) {
+	protected boolean deleteElementInModel(Object instance) throws EolRuntimeException {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public boolean store() {
-		throw new UnsupportedOperationException();
+	protected Object getCacheKeyForType(String type) throws EolModelElementTypeNotFoundException {
+		return getTypeResourceByName(type);
 	}
 
-	//
-	// JENA REASONER
+	@Override
+	public Collection<String> getAllTypeNamesOf(Object instance) {
+		List<String> types = new ArrayList<>();
 
-	public enum ReasonerType {
-		// TODO add to this list to cover reasoner types in the ReasonerRegistry Class
-		NONE, OWL_FULL;
+		if (instance instanceof RDFResource) {
+			RDFResource res = (RDFResource) instance;
+			for (RDFResource t : res.getTypes()) {
+				types.add(getQualifiedTypeName(t));
+			}
+		}
+
+		return types;
 	}
 
-	protected ReasonerType reasonerType = ReasonerType.NONE;
-
-	public ReasonerType getReasonerType() {
-		return reasonerType;
-	}
-
-	public void setReasonerType(ReasonerType rdfsReasonerType) {
-		this.reasonerType = rdfsReasonerType;
-	}
-	
-	//
-	// PROPERTY DATA URIS
-	
-	protected final List<String> dataURIs = new ArrayList<>();
-	
 	public List<String> getDataUris() {
 		return dataURIs;
 	}
@@ -374,56 +429,23 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 		this.dataURIs.add(uri);
 	}
 
-	//
-	// PROPERTY SCHEMA URIS
-	
-	protected final List<String> schemaURIs = new ArrayList<>();
-	
 	public List<String> getSchemaUris() {
 		return schemaURIs;
 	}
-	
 
 	public void setSchemaUri(String uri) {
 		this.schemaURIs.clear();
 		this.schemaURIs.add(uri);
 	}
 
-
-	//
-	// PROPERTY PREFIXES
-	
-	/** 
-	 * One of the keys used to construct the first argument to
-	 * {@link #load(StringProperties, String)}.
-	 *
-	 * This key should be set to a comma-separated list of prefix=uri pairs that
-	 * should be added to the prefix->URI map of the loaded RDF resource. These
-	 * pairs will take precedence over existing pairs in the resource.
-	 */
-	protected final Map<String, String> customPrefixesMap = new HashMap<>();
-	
-	private void loadPropertyPrefixes (StringProperties properties) {
-		this.customPrefixesMap.clear();
-		String sPrefixes = properties.getProperty(PROPERTY_PREFIXES, "").strip();
-		if (!sPrefixes.isEmpty()) {
-			for (String sItem : sPrefixes.split(",")) {
-				int idxEquals = sItem.indexOf('=');
-				if (idxEquals <= 0 || idxEquals == sItem.length() - 1) {
-					throw new IllegalArgumentException(String.format("Entry '%s' does not follow the prefix=uri format", sItem));
-				}
-				String sPrefix = sItem.substring(0, idxEquals);
-				String sURI = sItem.substring(idxEquals + 1);
-				customPrefixesMap.put(sPrefix, sURI);
-			}
-		}
-	}
-	
 	public Map<String, String> getCustomPrefixesMap() {
 		return this.customPrefixesMap;
 	}
 	
-	
+	public List<String> getLanguagePreference() {
+		return languagePreference;
+	}
+
 	/**
 	 * <p>
 	 * Returns the URI associated to a prefix.
@@ -443,7 +465,6 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 		return uri;
 	}
 
-	
 	/**
 	 * <p>
 	 * Returns a prefix associated with a namespace URI, if it exists.
@@ -463,76 +484,10 @@ public class RDFModel extends CachedModel<RDFModelElement> {
 		return model.getNsURIPrefix(namespaceURI);
 	}
 
-
-	//
-	// PROPERTY LANGUAGE PREFERENCE
-	
-
-	protected final List<String> languagePreference = new ArrayList<>();
-
-	private void loadPropertyLanguagePreference(StringProperties properties) throws EolModelLoadingException {
-		this.languagePreference.clear();
-		String sLanguagePreference = properties.getProperty(PROPERTY_LANGUAGE_PREFERENCE, "").strip();
-		if (!sLanguagePreference.isEmpty()) {
-			for (String tag : sLanguagePreference.split(",")) {
-				tag = tag.strip();
-				if (isValidLanguageTag(tag)) {
-					this.languagePreference.add(tag);
-				} else {
-					throw new EolModelLoadingException(
-						new IllegalArgumentException(
-							String.format("'%s' is not a valid BCP 47 tag", tag)
-						), this);
-				}
-			}
-		}
-	}
-	
-	public List<String> getLanguagePreference() {
-		return languagePreference;
-	}
-	
-
 	// Using Java's Locale class to check that tags conform to bcp47 structure
 	public static boolean isValidLanguageTag (String bcp47tag) {
 		boolean isValidBCP47 = !("und".equals(Locale.forLanguageTag(bcp47tag).toLanguageTag()));
 		return isValidBCP47;
 	}
-	
-	//
-	// PROPERTY JANA VALIDATE MODEL 
-	
-	protected String validationProperty = VALIDATION_SELECTION_DEFAULT;
-	
-	private void loadPropertyValidateModel(StringProperties properties) {
-		validationProperty = properties.getProperty(RDFModel.PROPERTY_VALIDATE_MODEL, VALIDATION_SELECTION_JENA);
-	}
-	
-	public String getValidationProperty () {
-		return validationProperty;
-	}
-	
-	//
-	// UNSUPPORTED OPERATIONS
-	
-	@Override
-	protected RDFResource createInstanceInModel(String type) throws EolModelElementTypeNotFoundException, EolNotInstantiableModelElementTypeException {
-		throw new UnsupportedOperationException();
-	}
-	
-	@Override
-	public void setElementId(Object instance, String newId) {
-		throw new UnsupportedOperationException();
-	}
-	
-	@Override
-	protected boolean deleteElementInModel(Object instance) throws EolRuntimeException {
-		throw new UnsupportedOperationException();
-	}
-	
-	@Override
-	public Object getEnumerationValue(String enumeration, String label) throws EolEnumerationValueNotFoundException {
-		throw new UnsupportedOperationException();
-	}	
 
 }
