1a763816eb8820d13cdf5b7c1e6cd1571a76aecf
Antonio Garcia-Dominguez <a.garcia-dominguez@york.ac.uk>
2025-09-30 18:46:27 +0100

    Add license headers

--- bundles/org.eclipse.epsilon.rdf.emf.converter/src/org/eclipse/epsilon/rdf/emf/converter/XMI2RDFHandler.java
+++ bundles/org.eclipse.epsilon.rdf.emf.converter/src/org/eclipse/epsilon/rdf/emf/converter/XMI2RDFHandler.java
@@ -1,4 +1,17 @@
+/********************************************************************************
+ * Copyright (c) 2025 University of York
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Antonio Garcia-Dominguez - initial API and implementation
+ ********************************************************************************/
 package org.eclipse.epsilon.rdf.emf.converter;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
